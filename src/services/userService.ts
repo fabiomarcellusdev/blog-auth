bc1930d5c94e76d6ffc8fd3fb422049d81279653
fabiomarcellusdev <166572074+fabiomarcellusdev@users.noreply.github.com>
2025-03-14 17:19:35 -0500

    Create user controller and service for registering user via email

---
+++ src/services/userService.ts
@@ -0,0 +1,17 @@
+import argon2 from "argon2";
+import User from "../models/User";
+
+export const registerUserWithEmail = async (name: string, email: string, password: string): Promise<void> => {
+    if(!name || !email || !password) {
+        throw new Error("Name, Email, and Password are all required.");
+    }
+
+    const existingUser = await User.findOne({ email });
+    if(existingUser) {
+        throw new Error("Email already in use.");
+    }
+
+    const hashedPassword = await argon2.hash(password);
+
+    await new User({ name, email, password: hashedPassword }).save();
+}
