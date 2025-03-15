95cddea517feb05974d0dd25a4ab6a2f0f1047f8
fabiomarcellusdev <166572074+fabiomarcellusdev@users.noreply.github.com>
2025-03-15 17:16:42 -0500

    Enable timestamps for userSchema

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,7 +7,7 @@ const userSchema = new Schema<IUser>({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String}
-});
+}, { timestamps: true});
 
 const User: Model<IUser> = model<IUser>("User", userSchema);
 
