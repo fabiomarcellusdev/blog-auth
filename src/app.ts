f662abd34df36a3bb1f6adc196f3a54f8a8cce75
fabiomarcellusdev <166572074+fabiomarcellusdev@users.noreply.github.com>
2025-03-15 17:17:48 -0500

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import passport from "./config/passport";
+
+import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoutes";
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
+app.use(passport.initialize());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+
+// Global Error Handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).json({ error: "Internal Server Error" });
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    if (!req.secure) {
+      return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
+  });
+
+export default app;
