420d4c544908a4f22baa0f4fd360fba6107c706d
fabiomarcellusdev <166572074+fabiomarcellusdev@users.noreply.github.com>
2025-03-16 15:25:52 -0500

    Reference user entity in userService and update methods

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,18 +1,30 @@
 import argon2 from "argon2";
-import User from "../models/User";
+import { User } from "../entity/User";
 import { ResponseError } from "../types/responseError";
 
-export const registerUserWithEmail = async (name: string, email: string, password: string): Promise<void> => {
-    if(!name || !email || !password) {
-        throw new ResponseError(400, "Name, Email, and Password are all required.");
+export const registerUserWithEmail = async (
+    name: string,
+    email: string,
+    password: string
+): Promise<void> => {
+    if (!name || !email || !password) {
+        throw new ResponseError(
+            400,
+            "Name, Email, and Password are all required."
+        );
     }
 
-    const existingUser = await User.findOne({ email });
-    if(existingUser) {
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
         throw new ResponseError(400, "Email already in use.");
     }
 
     const hashedPassword = await argon2.hash(password);
 
-    await new User({ name, email, password: hashedPassword }).save();
-}
+    const user = new User();
+    user.name = name;
+    user.email = email;
+    user.password = hashedPassword;
+
+    await user.save();
+};
