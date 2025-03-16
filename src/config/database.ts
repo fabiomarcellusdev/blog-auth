d36a1267cc5bb423233cb0b36d1513190bce5fca
fabiomarcellusdev <166572074+fabiomarcellusdev@users.noreply.github.com>
2025-03-16 17:30:25 -0500

    Enable logging in database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,27 +3,27 @@ import { DataSource } from "typeorm";
 require("dotenv").config();
 
 const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT as string, 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  synchronize: true,
-  logging: false,
-  entities: ["src/entity/**/*.ts"],
-  migrations: ["src/migration/**/*.ts"],
-  subscribers: ["src/subscriber/**/*.ts"],
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT as string, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: true,
+    entities: ["src/entity/**/*.ts"],
+    migrations: ["src/migration/**/*.ts"],
+    subscribers: ["src/subscriber/**/*.ts"],
 });
 
 const connectDB = async () => {
-  try {
-    await AppDataSource.initialize();
-    console.log("PostgreSQL connected");
-  } catch (error) {
-    console.error("PostgreSQL connection error:", error);
-    process.exit(1);
-  }
+    try {
+        await AppDataSource.initialize();
+        console.log("PostgreSQL connected");
+    } catch (error) {
+        console.error("PostgreSQL connection error:", error);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
