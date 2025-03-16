5eeb23c1f2ac9ea9be87c7374e058633b00062fa
fabiomarcellusdev <166572074+fabiomarcellusdev@users.noreply.github.com>
2025-03-16 14:30:49 -0500

    Update database.ts to use PostgreSQL config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,16 +1,30 @@
-import mongoose from 'mongoose';
-require('dotenv').config();
+import { DataSource } from "typeorm";
+
+require("dotenv").config();
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT as string, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+});
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URL as string, {
-        sanitizeFilter: true,
-        });
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-        process.exit(1);
-    }
-}
+  try {
+    await AppDataSource.initialize();
+    console.log("PostgreSQL connected");
+  } catch (error) {
+    console.error("PostgreSQL connection error:", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
+export { AppDataSource };
