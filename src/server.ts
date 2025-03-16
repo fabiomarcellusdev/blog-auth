cb96f4d0cb7145d4be8605abbcb79cf18664032e
fabiomarcellusdev <166572074+fabiomarcellusdev@users.noreply.github.com>
2025-03-16 15:25:35 -0500

    Import reflect-metadata inside server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,24 @@
 import express from "express";
 import connectDB from "./config/database";
-import fs from 'fs';
-import https from 'https';
+import fs from "fs";
+import https from "https";
+import "reflect-metadata";
 
 require("dotenv").config();
 
 const options = {
-    key: fs.readFileSync(process.env.SSL_KEY_PATH as string),
-    cert: fs.readFileSync(process.env.SSL_CERT_PATH as string)
-}
+  key: fs.readFileSync(process.env.SSL_KEY_PATH as string),
+  cert: fs.readFileSync(process.env.SSL_CERT_PATH as string),
+};
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 const startServer = async () => {
-    await connectDB();
-    https.createServer(options, app).listen(PORT, () => {
-        console.log(`Secure server running on port ${PORT} with HTTPS`);
-    })
-}
+  await connectDB();
+  https.createServer(options, app).listen(PORT, () => {
+    console.log(`Secure server running on port ${PORT} with HTTPS`);
+  });
+};
 
 startServer();
-
