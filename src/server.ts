69ccd4a4bbb649f34c3a000f1bdd9d44a1e12b14
fabiomarcellusdev <166572074+fabiomarcellusdev@users.noreply.github.com>
2025-03-17 16:59:09 -0500

    Remove HTTPS code

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,17 @@
 import express from "express";
 import connectDB from "./config/database";
-import fs from "fs";
-import https from "https";
 import "reflect-metadata";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const options = {
-    key: fs.readFileSync(process.env.SSL_KEY_PATH as string),
-    cert: fs.readFileSync(process.env.SSL_CERT_PATH as string),
-};
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 const startServer = async () => {
     await connectDB();
-    https.createServer(options, app).listen(PORT, () => {
-        console.log(`Secure server running on port ${PORT} with HTTPS`);
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
     });
 };
 
