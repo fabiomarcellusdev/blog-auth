9f00433b19d7b50cedd189360f203582a30a93b7
fabiomarcellusdev <166572074+fabiomarcellusdev@users.noreply.github.com>
2025-03-19 17:36:11 -0500

    Add constants for success and error messages

---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+export const ERROR_MESSAGES = {
+    REGISTER_DETAILS_REQUIRED: "Name, Email, and Password are all required.",
+    LOGIN_DETAILS_REQUIRED: "Email and Password are required.",
+    EMAIL_ALREADY_IN_USE: "Email already in use.",
+    INVALID_EMAIL_OR_PASSWORD: "Invalid email or password.",
+    PROVIDER_ACCOUNT: (provider: string) =>
+        `This email is associated with a ${provider} account. Please log in using ${provider}.`,
+    DATABASE_ERROR: "Database error occurred.",
+    INTERNAL_SERVER_ERROR: "Internal server error.",
+    INVALID_TOKEN: "Invalid Token.",
+    FAILED_TO_SEND_EMAIL: "Failed to send email.",
+};
+
+export const SUCCESS_MESSAGES = {
+    USER_REGISTERED: "User registered successfully.",
+    USER_LOGGED_IN: "User successfully logged in.",
+    USER_UPDATED: "User updated successfully.",
+    USER_DELETED: "User deleted successfully.",
+    EMAIL_VERIFIED: "Email verified successfully.",
+};
